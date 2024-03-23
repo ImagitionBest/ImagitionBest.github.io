7a60e270fd4ef89920cb4b633248595041f56d9e
ImagitionBest <2313414538@qq.com>
2024-03-23 15:18:10 +0800

    Site updated: 2024-03-23 15:18:10

--- js/random.js
+++ js/random.js
@@ -1,11 +1,1 @@
-hexo.extend.generator.register('random', function (locals) {
-    const config = hexo.config.random || {}
-    const posts = []
-    for (const post of locals.posts.data) {
-        if (post.random !== false) posts.push(post.path)
-    }
-    return {
-        path: config.path || '/js/random.js',
-        data: `var posts=${JSON.stringify(posts)};function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};`
-    }
-})
+var posts=["2024/03/18/hello-world/","2024/03/21/第1篇博客：Markdown初级语法/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
