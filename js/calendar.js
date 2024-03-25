ecd02c5db601f7e6a0f6dfc5ddca3a2ceeb75d56
ImagitionBest <2313414538@qq.com>
2024-03-25 16:55:57 +0800

    Site updated: 2024-03-25 16:55:57

--- js/calendar.js
+++ js/calendar.js
@@ -375,9 +375,8 @@
     root: '/calendar/',
     url: '/calendar.json'
   };
-
+  $(document).ready(function () {
+    $('#calendar').aCalendar('zh-CN');//'zh-CN'请根据自己博客的语言选择
+  });  
 }(jQuery));
 
-$(document).ready(function () {
-  $('#calendar').aCalendar('zh-CN');//'zh-CN'请根据自己博客的语言选择
-});
