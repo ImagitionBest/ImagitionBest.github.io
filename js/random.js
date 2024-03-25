9f8e48798fc9df8d418239827e9538bdd20c28de
ImagitionBest <2313414538@qq.com>
2024-03-25 14:37:24 +0800

    Site updated: 2024-03-25 14:37:24

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/03/18/hello-world/","2024/03/21/第1篇博客：Markdown初级语法/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2024/03/21/第1篇博客：Markdown初级语法/","2024/03/18/hello-world/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
