316a43240f89c7fdd394dfd50afc5d0bb52013c1
ImagitionBest <2313414538@qq.com>
2024-04-08 10:52:20 +0800

    Site updated: 2024-04-08 10:52:19

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/04/07/第3篇博客-利用Github-jsDelivr搭建高速图床/","2024/03/21/第1篇博客：Markdown初级语法/","2024/03/18/第2篇博客：标签外挂/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2024/04/07/第3篇博客-利用Github-jsDelivr搭建高速图床/","2024/04/08/第4篇博客-Butterfly主题添加微博热搜/","2024/03/21/第1篇博客：Markdown初级语法/","2024/03/18/第2篇博客：标签外挂/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
